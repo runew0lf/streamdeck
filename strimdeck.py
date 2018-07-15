3f08577c449c8ae01615492f2ad2283d66f53d8f
runew0lf <runew0lf@gmail.com>
2018-07-15 15:12:29 +0100

    Added gitignore and requirements

--- strimdeck.py
+++ strimdeck.py
@@ -5,16 +5,44 @@ import pyautogui
 import pyperclip
 import subprocess
 from os import listdir
-from streamdeck_image import get_key_alt_image, get_key_image
+from streamdeck_image import get_key_alt_image, get_key_image, get_key_full_image
+
+from PIL import Image, GifImagePlugin
+import io
+import time
 
 key_images = []
-# key_images = get_key_full_image('full.png')
 key_alt_images = []
 for k in range(15):
     # Choose a random icon and assign it to a key
     key_images.append(get_key_image(f"icons\{random.choice(listdir('icons'))}"))
     # Generate default alt images (when key is pressed)
     key_alt_images.append(get_key_alt_image(key_images[k]))
+
+#key_images = get_key_full_image('full.png')
+
+
+def test():
+    image = Image.open("test.gif")
+
+    for frame in range(0, image.n_frames):
+        output = io.BytesIO()
+        image.seek(frame)
+        wpercent = (72 / float(image.size[0]))
+        hsize = int((float(image.size[1]) * float(wpercent)))
+        rescale = image.resize((72, hsize), Image.ANTIALIAS)
+        img_w, img_h = rescale.size
+        background = Image.new('RGBA', (72, 72), (0, 0, 0, 255))
+        bg_w, bg_h = background.size
+        offset = ((bg_w - img_w) // 2, (bg_h - img_h) // 2)
+        background.paste(rescale, offset)
+        background = background.rotate(180)
+        background.convert('RGB').save(output, 'BMP')
+
+        streamdeck.set_key_image(0, output.getvalue()[54:])
+        output.close()
+        time.sleep(0.03)
+
 
 
 def key_change_callback(deck, key, state):
@@ -28,6 +56,7 @@ def key_change_callback(deck, key, state):
             subprocess.call(args)
 
     else:
+        test()
         deck.set_key_image(key, key_images[key])
 
 
@@ -41,6 +70,7 @@ if __name__ == "__main__":
     for k in range(streamdeck.key_count()):
         streamdeck.set_key_image(k, key_images[k])
 
+    test()
     streamdeck.set_key_callback(key_change_callback)
 
     for t in threading.enumerate():
@@ -48,3 +78,4 @@ if __name__ == "__main__":
             continue
 
         t.join()
+
